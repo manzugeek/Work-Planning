0e397a893b39b877d793d8d759b14b82774b8f4c
khamis manzu <kmanzu@yahoo.com>
2023-01-02 18:50:46 +0300

    add nested route for user and worker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { registrations: "registrations" }
+
+  resources :users do
+    resources :workers
+  end
 
   # root "articles#index"
 end
