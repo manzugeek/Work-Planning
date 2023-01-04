7ea4187ce2f0dea577b7ac88cd4f7798a0836034
khamis manzu <kmanzu@yahoo.com>
2023-01-04 11:04:42 +0300

    add shift model

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,7 +1,22 @@
 class Worker < ApplicationRecord
-  belongs_to :user
-
+  
   validates :title, presence: true
   validates :dept, presence: true
+  validates :no_overlapping_shifts
+
+  belongs_to :user
+  has_many :shifts
+
+  validates :no_overlapping_shifts
+
+  def no_overlapping_shifts
+    # Get all shifts for this worker on the same day
+    worker_shifts = shifts.where("start_time BETWEEN ? AND ?", Date.today.beginning_of_day, Date.today.end_of_day)
+
+    # Check if there is more than one shift on the same day
+    if worker_shifts.count > 1
+      errors.add(:shifts, "cannot overlap")
+    end
+  end
 
 end
